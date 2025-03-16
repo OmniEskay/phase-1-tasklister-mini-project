ba021383b353251a80ae607270a6c3db6421507c
+ <omnieskay@proton.me>
2025-03-16 19:44:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  
+  taskForm.addEventListener("submit", (event) => {
+      event.preventDefault(); 
+      
+      const taskInput = document.getElementById("new-task-description");
+      const taskValue = taskInput.value.trim();
+      
+      if (taskValue === "") return; // stop the addition of empty tasks
+      
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskValue;
+      
+      
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "X";
+      deleteButton.addEventListener("click", () => {
+          taskItem.remove();
+      });
+      
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+      
+      taskInput.value = "";
+  });
 });
